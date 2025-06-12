ef92173ddfb5752c3602672d0ca011b2a019d0f1
郑伟超 <zhengwc09@chinatelecom.cn>
2025-06-12 17:24:33 +0800

    添加ignore

--- Day01-20/salary/employee.py
+++ Day01-20/salary/employee.py
@@ -42,11 +42,17 @@ class Salesman(Employee):
         return 1800.0 + self.sales * 0.05
 
 
-if __name__ == '__main__':
-    emps = [Manager('刘备'), Programmer('诸葛亮'), Manager('曹操'), Programmer('荀彧'), Salesman('张辽')]
+if __name__ == "__main__":
+    emps = [
+        Manager("刘备"),
+        Programmer("诸葛亮"),
+        Manager("曹操"),
+        Programmer("荀彧"),
+        Salesman("张辽"),
+    ]
     for emp in emps:
         if isinstance(emp, Programmer):
-            emp.working_hours = int(input(f'请输入{emp.name}本月工作时间: '))
+            emp.working_hours = int(input(f"请输入{emp.name}本月工作时间: "))
         elif isinstance(emp, Salesman):
-            emp.sales = float(input(f'请输入{emp.name}本月销售额: '))
-        print(f'{emp.name}本月工资为: ￥{emp.get_salary():.2f}元')
+            emp.sales = float(input(f"请输入{emp.name}本月销售额: "))
+        print(f"{emp.name}本月工资为: ￥{emp.get_salary():.2f}元")
